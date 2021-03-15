c0684ac7e3d69a1fd6ccadb3e198c06357b5c8ae
mogki <mogki@naver.com>
2021-03-15 11:12:11 +0900

    sudo

--- app/start.sh
+++ app/start.sh
@@ -3,6 +3,6 @@
 sudo yum install -y nodejs docker git
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
-service docker start
+sudo service docker start
 
-docker-compose up -d --build
+sudo docker-compose up -d --build
